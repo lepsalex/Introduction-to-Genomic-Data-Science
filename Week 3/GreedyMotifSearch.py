c2528f7142faa04fe1a943556b52f049ccd73af7
Alex <lepsalex@gmail.com>
2018-12-25 10:55:36 -0500

    Starting on GreedyMotifSearch

---
+++ Week 3/GreedyMotifSearch.py
@@ -0,0 +1,5 @@
+from functools import reduce
+
+# Implement GreedyMotifSearch() below along with any subroutines that you need.
+# Your function should return a list of strings.
+def GreedyMotifSearch(Dna, k, t):
