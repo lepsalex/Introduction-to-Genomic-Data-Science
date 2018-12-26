2f3ebe34988eae5af96cda612dec066a579c897b
Alex <lepsalex@gmail.com>
2018-12-26 10:22:51 -0500

    Starting RandomizedMotifSearch

---
+++ Week 4/RandomizedMotifSearch.py
@@ -0,0 +1,8 @@
+# Import the random package, then write a function RandomizedMotifSearch() here along with any subroutines you need.
+# RandomizedMotifSearch() should return a list of strings.
+def RandomizedMotifSearch(Dna, k, t):
+
+# Then, write a function here called RepeatedRandomizedMotifSearch() that takes Dna, k, t, and a parameter
+# N that returns the best collection of motifs resulting from running RandomizedMotifSearch()
+# N times.  It should return a list of strings.
+def RepeatedRandomizedMotifSearch(Dna, k, t, N):
